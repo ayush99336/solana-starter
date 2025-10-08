9376fb1441e8930636abac5d04fc6b46750feeca
ayush99336 <ayushkaushikhere@gmail.com>
2025-10-08 18:12:03 +0530

    mint done

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,6 +1,6 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
-import { createMint } from '@solana/spl-token';
-import wallet from "../turbin3-wallet.json"
+import { createMint } from "@solana/spl-token";
+import wallet from "../../turbin3-wallet.json"; // 9gXWuNuZR34uCvmqR2prGEQ22p9KxcmDhBywd7P1a7yf
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -10,10 +10,22 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 (async () => {
-    try {
-        // Start here
-        // const mint = ???
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
-    }
-})()
+  try {
+    // Start here
+    // const mint = ???
+    const mint = await createMint(
+      connection, // connection
+      keypair, // payer
+      keypair.publicKey, // mint authority
+      null, // freeze authority (optional)
+      6 // decimals
+    );
+    console.log(`Mint Address: ${mint.toBase58()}`);
+
+    console.log(`Mint created successfully! with {decimals: 6} at ${mint}`);
+    // Mint Address: 9kPydm89QEcE4aJ3SV2zEcS6HiCmD8ozHJcHkmRtEtcQ
+    // Mint created successfully! with {decimals: 6} at 9kPydm89QEcE4aJ3SV2zEcS6HiCmD8ozHJcHkmRtEtcQ
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+  }
+})();
--- ts/cluster1/spl_mint.ts
+++ ts/cluster1/spl_mint.ts
@@ -1,6 +1,7 @@
 import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
-import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
-import wallet from "../turbin3-wallet.json"
+import { getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
+import wallet from "../../turbin3-wallet.json";
+import { get } from "prompt";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -12,18 +13,37 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
 const token_decimals = 1_000_000n;
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("9kPydm89QEcE4aJ3SV2zEcS6HiCmD8ozHJcHkmRtEtcQ");
 
 (async () => {
-    try {
-        // Create an ATA
-        // const ata = ???
-        // console.log(`Your ata is: ${ata.address.toBase58()}`);
+  try {
+    // Create an ATA
+    console.log(`Creating ATA for ${keypair.publicKey.toBase58()}`);
+    const ata = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair, // payer
+      mint, // mint
+      keypair.publicKey // owner
+    );
+    console.log(`Your ata is: ${ata.address.toBase58()}`);
 
-        // Mint to ATA
-        // const mintTx = ???
-        // console.log(`Your mint txid: ${mintTx}`);
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
-    }
-})()
+    // Mint to ATA
+    const mintTx = await mintTo(
+      connection,
+      keypair,
+      mint,
+      ata.address,
+      keypair,
+      1_000_000n
+    );
+    console.log(`Minted 1000000 units = 1 tokens to ${ata.address.toBase58()}`);
+    console.log(`Your mint txid: ${mintTx}`);
+
+    // Creating ATA for 9gXWuNuZR34uCvmqR2prGEQ22p9KxcmDhBywd7P1a7yf
+    // Your ata is: 5VrE5jcm6tMp4X3ytpCEpt4Kz93GcBtmSfqh5JfrvLKi
+    // Minted 1000000 tokens to 5VrE5jcm6tMp4X3ytpCEpt4Kz93GcBtmSfqh5JfrvLKi
+    // Your mint txid: 54hvX3RxNaX7jLkCdaoaStUtzbHQN1et4ZswbsckN1MuE7QR5TmLB2Ws3NLv5BWot1rrW55WDQvYoUzmAmdKraCP
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+  }
+})();
